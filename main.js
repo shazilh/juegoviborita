3a876f36d14b0ccac9a4608adf50cf5c4ad69202
Shazil Tovar <shazil@gmail.com>
2017-04-09 14:35:27 -0500

    agregado nuevas funciones

--- main.js
+++ main.js
@@ -1,27 +1,72 @@
-var contenedor=document.getElementById('contenedor');
 document.addEventListener("keydown",moverSerpiente);
 
-function moverSerpiente(event){
-  var serpiente=document.getElementById('cabezaSerpiente');
-  var tecla = event.keyCode;
-  var posicionY= serpiente.offsetLeft;
-  var posicionX=serpiente.offsetTop;
-    switch (tecla){
-      case 37:
-        serpiente.style.left=posicionY-100 + "px";
-        if(posicionY<100){
-          alert("Estas fuera del juego");
+var contenedor=document.getElementById('contenedor');
+var serpiente=document.getElementById('cabezaSerpiente');
+var boton=document.getElementById("reiniciar");
+boton.addEventListener("click",reiniciar);
+
+//boton.addEventListener("click",reiniciar);
+var posicionX=0;
+var posicionY=0;
+var movimiento=79;
+var inicializar=0;
+function moverSerpiente(evento){
+  //var posicionY= serpiente.offsetLeft;
+  //var posicionX=serpiente.offsetTop;
+  switch (evento.keyCode){
+      case 38://mover izquierda
+        posicionY=posicionY-movimiento;
+        if(posicionY<0){
+          perdiste();
+          dejardeMover()
+
+        }else{
+          serpiente.style.marginTop=posicionY.toString() + "px";
         }
-        break;
-      case 38:
-        serpiente.style.top=posicionX-100 + "px";
-        break;
-      case 39:
-        serpiente.style.left=posicionY+100 + "px";
-        break;
-      case 40:
-          serpiente.style.top=posicionX+100+ "px";
-          break;
+      break;
+
+      case 40://mover derecha
+        posicionY=posicionY+movimiento;
+        if(posicionY>395){
+            perdiste();
+            dejardeMover()
+        } else{
+          serpiente.style.marginTop = posicionY.toString() + "px";
+        }
+      break;
+      case 37://mover arriba
+        posicionX=posicionX-movimiento;
+          if(posicionX<0){
+              perdiste();
+              dejardeMover()
+          }else{
+            serpiente.style.marginLeft = posicionX.toString() + "px";
+          }
+      break;
+      case 39://mover abajo
+          posicionX=posicionX+movimiento;
+          if(posicionX>395){
+            perdiste();
+            dejardeMover()
+          } else{
+            serpiente.style.marginLeft= posicionX.toString() + "px";
+          }
+      break;
       default:
+        console.log("otra tecla");
+      break;
     }
+}
+
+function perdiste(){
+  alert("Estas fuera del juego");
+}
+function dejardeMover(){
+  document.removeEventListener("keydown", moverSerpiente);
+}
+function reiniciar(){
+  serpiente.style.marginLeft = (inicializar +"px").toString();
+  serpiente.style.marginTop = (inicializar + "px").toString();
+
+  document.addEventListener("keydown", moverSerpiente)
 }
